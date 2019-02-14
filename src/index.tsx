708ea1add670625f2009c358c9ed3b6cb58be879
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-02-14 18:42:09 +0300

    Добавил пример с пары

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/ 1. A component! */
+class GreetingCard extends React.Component {
+  state = {
+    text: "Hello, World!"
+  };
+
+  handleClick = () => {
+    this.setState({ text: "Hello, ITIS" });
+  };
+
+  render() {
+    const { text: mytext } = this.state;
+    return (
+      <div>
+        <h1>{mytext}</h1>
+        <button onClick={this.handleClick}>Hey!</button>
+      </div>
+    );
+  }
+}
+
+/ 2. Define a container for the app */
+const container = document.getElementById("root");
+
+/** 3. Render the App */
+ReactDOM.render(<GreetingCard />, container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
