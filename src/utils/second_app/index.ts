3d298e317582f7ab9b71080b777d26ca301836d2
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-03-12 17:23:41 +0300

    Увеличил границы случайных чисел

--- src/utils/second_app/index.ts
+++ src/utils/second_app/index.ts
@@ -4,7 +4,7 @@ export default class UserCardUtils {
     static getRandomNumber(){
 
         const min = 1;
-        const max = 20;
+        const max = 10000;
 
         return  Math.floor(Math.random() * (max - min + 1)) + min;
 
